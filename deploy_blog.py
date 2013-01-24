fc148a93340e1b8af83e44588714b54dd0116553
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-01-24 11:08:24 -0200

    Deploy at 2013-01-24

--- deploy_blog.py
+++ deploy_blog.py
@@ -4,7 +4,7 @@ from datetime import date
 msg_commit = "git commit -am 'Deploy at "+ str(date.today()) + "'"
 
 os.system("rm -rf ../viniciusmo.github.com/*")
-subprocess.call("cp public/* -r ../viniciusmo.github.com/", shell=True)
+subprocess.call("cp -r public/* ../viniciusmo.github.com/", shell=True)
 os.chdir("../viniciusmo.github.com/")
 os.system("git add .")
 os.system(msg_commit)
