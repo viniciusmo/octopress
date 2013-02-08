c9d03211a83cb274cb5bee54b5e68d49c95fc0b9
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-02-08 18:27:21 -0200

    Deploy at 2013-02-08

--- scripts/download_code.py
+++ scripts/download_code.py
@@ -5,8 +5,15 @@ xmldoc = minidom.parse('scripts/data/download_code.xml')
 itemlist = xmldoc.getElementsByTagName('item') 
 for s in itemlist :
     url = s.firstChild.nodeValue
-    response = urllib2.urlopen(url).read()
-    print "Downloaing..."
+    try:
+    	urllib.urlcleanup();
+    	response = urllib2.urlopen(url).read()
+    	print "Downloaing..."
+    except urllib2.HTTPError, e:
+    	print e.code
+    	print e.msg
+    	print e.headers
+
     print url
     f = open('source/downloads/code/'+s.attributes['name'].value,'w')
     f.write(response)
